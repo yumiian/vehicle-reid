6996b49ba7542538cc5bd4bf4c459d86aa4acc94
michael <tutmas156@gmail.com>
2025-03-30 20:38:01 +0800

    Add probability beside abbreviation in augment

--- gui/augment.py
+++ gui/augment.py
@@ -3,28 +3,18 @@ import cv2
 import os
 
 def get_abbreviation(transform_list):
-    """Generate abbreviation for output filename based on selected augmentations."""
-    abbr = ""
-    for transform in transform_list:
-        if isinstance(transform, A.HorizontalFlip):
-            abbr += "HF_"
-        elif isinstance(transform, A.VerticalFlip):
-            abbr += "VF_"
-        elif isinstance(transform, A.RandomRotate90):
-            abbr += "RR90_"
-        elif isinstance(transform, A.Erasing):
-            abbr += "ERS_"
-        elif isinstance(transform, A.ColorJitter):
-            abbr += "CJ_"
-        elif isinstance(transform, A.Sharpen):
-            abbr += "SHRP_"
-        elif isinstance(transform, A.UnsharpMask):
-            abbr += "UNSM_"
-        elif isinstance(transform, A.CLAHE):
-            abbr += "CLAHE_"
-    return abbr
+    """Generate abbreviation based on selected augmentations and probabilities."""
+    abbr_map = {A.HorizontalFlip: "HF", A.VerticalFlip: "VF", A.RandomRotate90: "RR90", 
+                A.Erasing: "ERS", A.ColorJitter: "CJ", A.Sharpen: "SHRP", 
+                A.UnsharpMask: "UNSM", A.CLAHE: "CLAHE"}
 
-def augment(transform_list, seed, image_path):
+    return "_".join(
+        f"{abbr_map[type(t)]}{int(t.p * 100):02d}" for t in transform_list if type(t) in abbr_map
+    ) + "_" if transform_list else ""
+
+def augment(transform_list, seed, image_path, output_path):
+    os.makedirs(output_path, exist_ok=True)
+    
     transform = A.Compose(transform_list, seed=seed, strict=True)
     abbr = get_abbreviation(transform_list)
 
@@ -37,5 +27,6 @@ def augment(transform_list, seed, image_path):
         image = cv2.imread(filepath, cv2.COLOR_BGR2RGB)
         augmented_image = transform(image=image)["image"]
         
-        output_filename = os.path.join(image_path, f"{abbr}{filename}")
+        output_filename = os.path.join(output_path, f"{abbr}{filename}")
         cv2.imwrite(output_filename, augmented_image)
+        break
